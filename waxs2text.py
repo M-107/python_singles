76afe641e992de7f81783f0b1caa68fb47ed8e48
Martin M <martin.mrnak.107@gmail.com>
2023-05-07 17:04:53 +0200

    script that converts all waxs formats to txt file

---
+++ waxs2text.py
@@ -0,0 +1,39 @@
+from waxsreaders import *
+import tkinter as tk
+from tkinter.filedialog import askopenfilenames
+
+FORMATS = {
+    "asc" : get_xy_asc,
+    "itx" : get_xy_itx,
+    "ras" : get_xy_ras,
+    "scn" : get_xy_scn,
+    "xrdml" : get_xy_xrdml,
+}
+
+root = tk.Tk()
+root.withdraw()
+files = askopenfilenames(
+    title="Select the files with RTG data.",
+    filetypes=[("RTG data files", ".asc .itx .ras .scn .xrdml")],
+)
+
+for file in files:
+    print(f"Working on file: {file}")
+    new_file = ".".join(file.split(".")[:-1]) + ".txt"
+    filetype = file.split(".")[-1].lower()
+    x_list, y_list = FORMATS[filetype](file)
+    x_math_list = []
+    for x in x_list:
+        formatted = "{:.14E}".format(x)
+        parts = formatted.split("E+")
+        number = parts[0]
+        exponent = parts[1]
+        new_exponent = exponent.rjust(4, "0")
+        new_x = f"{number}E+{new_exponent}"
+        x_math_list.append(new_x)
+    xy_list = list(zip(x_math_list, y_list))
+    with open(new_file, 'w') as f:
+        for pair in xy_list:
+            x, y = pair
+            line = f"{x}  {y}\n"
+            f.write(line)
