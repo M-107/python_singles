4039786913febeda7831963bdf0503e6c7165228
Martin M <martin.mrnak.107@gmail.com>
2023-07-01 21:04:27 +0200

    sudoku solver (only hardcoded input for now)

---
+++ sudoku_solver.py
@@ -0,0 +1,104 @@
+import numpy as np
+
+class Board():
+    def __init__(self) -> None:
+        self.array = np.zeros([9,9])
+        self.init_array = np.zeros([9,9])
+
+    def __repr__(self):
+        rows, cols = self.array.shape
+        output = ""
+        for i in range(rows):
+            if i % 3 == 0 and i != 0:
+                output += "-" * (cols * 2 + 3) + "\n"
+            for j in range(cols):
+                if j % 3 == 0 and j != 0:
+                    output += "| "
+                output += str(int(self.array[i][j])).replace("0", "_") + " "
+            output += "\n"
+        return output
+
+    def set_state(self, input: list):
+        if len(input) != 9:
+            raise ValueError("Input list should contain 9 lists.")
+        for i in range(len(input)):
+            row = input[i]
+            if len(row) != 9:
+                raise ValueError("Each inner list should contain 9 elements.")
+            for j in range(len(row)):
+                value = row[j]
+                if value is None:
+                    self.array[i][j] = 0
+                    self.init_array[i][j] = 0
+                elif isinstance(value, int) and 1 <= value <= 9:
+                    self.array[i][j] = value
+                    self.init_array[i][j] = value
+                else:
+                    raise ValueError(f"Invalid value found in input list: {value} at R{i+1} C{j+1}")
+
+    def check(self):
+        for i in range(9):
+            row_values = [value for value in self.array[i] if value != 0]
+            col_values = [value for value in self.array[:, i] if value != 0]
+            if len(row_values) != len(set(row_values)):
+                return False
+            if len(col_values) != len(set(col_values)):
+                return False
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                subgrid_values = [
+                    value
+                    for value in self.array[i:i+3, j:j+3].flatten()
+                    if value != 0
+                ]
+                if len(subgrid_values) != len(set(subgrid_values)):
+                    return False
+        return True
+
+    def solve(self):
+        print("Solving the sudoku...")
+        if not self.check():
+            return False
+        return self._solve()
+
+    def _solve(self):
+        for i in range(9):
+            for j in range(9):
+                if self.array[i][j] == 0 and self.init_array[i][j] == 0:
+                    for num in range(1, 10):
+                        self.array[i][j] = num
+                        print(self, end="\033[F" * 11)
+                        if self.check():
+                            if self._solve():
+                                return True
+                    self.array[i][j] = 0
+                    return False
+        return True
+
+
+def main():
+    sudoku = Board()
+    initial_state = [
+        [5, 3, None, None, 7, None, None, None, None],
+        [6, None, None, 1, 9, 5, None, None, None],
+        [None, 9, 8, None, None, None, None, 6, None],
+        [8, None, None, None, 6, None, None, None, 3],
+        [4, None, None, 8, None, 3, None, None, 1],
+        [7, None, None, None, 2, None, None, None, 6],
+        [None, 6, None, None, None, None, 2, 8, None],
+        [None, None, None, 4, 1, 9, None, None, 5],
+        [None, None, None, None, 8, None, None, 7, 9]
+    ]
+    sudoku.set_state(initial_state)
+    print(">>> Initial State <<<")
+    print(sudoku)
+    if sudoku.solve():
+        print("\033[F"*2)
+        print(">>> Solved  State <<<")
+        print(sudoku)
+    else:
+        print("This sudoku has no solution")
+        print("\n" * 10)
+
+if __name__ == "__main__":
+    main()
