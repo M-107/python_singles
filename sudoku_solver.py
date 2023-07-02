ae361293244c62dd4add25e829c00f26b154d9ad
Martin M <martin.mrnak.107@gmail.com>
2023-07-02 13:30:38 +0200

    sudoku can be manually entered by user

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,5 +1,7 @@
 import numpy as np
 from time import perf_counter
+import keyboard
+import subprocess
 
 
 class Sudoku:
@@ -93,6 +95,45 @@ class Sudoku:
                         f"Invalid value found in input list: {value} at R{i+1} C{j+1}"
                     )
 
+    def set_manually(self):
+        current_x = 0
+        current_y = 0
+        is_key_pressed = False
+        print("    Input your sudoku    ")
+        while True:
+            if keyboard.is_pressed("up"):
+                if not is_key_pressed:
+                    current_x = max(current_x - 1, 0)
+                    is_key_pressed = True
+            elif keyboard.is_pressed("down"):
+                if not is_key_pressed:
+                    current_x = min(current_x + 1, 8)
+                    is_key_pressed = True
+            elif keyboard.is_pressed("left"):
+                if not is_key_pressed:
+                    current_y = max(current_y - 1, 0)
+                    is_key_pressed = True
+            elif keyboard.is_pressed("right"):
+                if not is_key_pressed:
+                    current_y = min(current_y + 1, 8)
+                    is_key_pressed = True
+            elif any(keyboard.is_pressed(str(n)) for n in range(0, 10)):
+                if not is_key_pressed:
+                    for n in range(0, 10):
+                        if keyboard.is_pressed(str(n)):
+                            self.array[current_x, current_y] = n
+                            self.init_array[current_x, current_y] = n
+                    is_key_pressed = True
+            elif keyboard.is_pressed("enter"):
+                if not is_key_pressed:
+                    is_key_pressed = True
+                    print(self.print_me(current_x, current_y), end="\033[F" * 15)
+                    break
+            else:
+                is_key_pressed = False
+            print(self.print_me(current_x, current_y), end="\033[F" * 13)
+
+
     def check(self):
         for i in range(9):
             row_values = [value for value in self.array[i] if value != 0]
@@ -135,7 +176,22 @@ class Sudoku:
         return True
 
 
-def main():
+def solve_timer(sudoku):
+    print("\n      Initial State      ")
+    print(sudoku)
+    t_start = perf_counter()
+    if sudoku.solve(True):
+        print("\033[F" * 2)
+        print("      Solved State       ")
+        print(sudoku)
+    else:
+        print("This sudoku has no solution")
+        print("\n" * 12)
+    t_end = perf_counter()
+    print(f"The solution took {sudoku.BLUE}{round(t_end - t_start, 2)}{sudoku.WHITE} seconds")
+
+
+def main_set_hardcoded():
     sudoku = Sudoku()
     initial_state = [
         [5, 3, None, None, 7, None, None, None, None],
@@ -149,19 +205,16 @@ def main():
         [None, None, None, None, 8, None, None, 7, 9],
     ]
     sudoku.set_state(initial_state)
-    print("\n      Initial State      ")
-    print(sudoku)
-    t_start = perf_counter()
-    if sudoku.solve(True):
-        print("\033[F" * 2)
-        print("      Solved State       ")
-        print(sudoku)
-    else:
-        print("This sudoku has no solution")
-        print("\n" * 10)
-    t_end = perf_counter()
-    print(f"The solution took {sudoku.BLUE}{round(t_end - t_start, 2)}{sudoku.WHITE} seconds")
+    solve_timer(sudoku)
+
+
+def main_set_manually():
+    sudoku = Sudoku()
+    sudoku.set_manually()
+    solve_timer(sudoku)
+    subprocess.run(["timeout", "1"], capture_output=True)
 
 
 if __name__ == "__main__":
-    main()
+    # main_set_hardcoded()
+    main_set_manually()
