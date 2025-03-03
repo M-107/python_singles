10452ac7052d5c2c8425b3879d04954fdc5482c5
Martin Mrnak <martin.mrnak.107@gmail.com>
2025-03-03 23:03:31 +0100

    a2p now sorts songs by most likely order to be played at a show

--- spotify/artist2playlist.py
+++ spotify/artist2playlist.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup
 from spotipy.oauth2 import SpotifyOAuth
 
 
-def init_spotify():
+def init_spotify() -> spotipy.Spotify:
     spotify = spotipy.Spotify(
         auth_manager=SpotifyOAuth(
             scope="playlist-modify-public",
@@ -21,14 +21,12 @@ def init_spotify():
     return spotify
 
 
-def get_artist_songs(artist: str):
-    print(f"Searching for {artist}...")
+def get_artist_songs(artist: str) -> list[str]:
     search_query = artist.lower().replace(" ", "+")
     page_url = f"https://www.setlist.fm/search?query={search_query}"
     page_response = requests.get(page_url)
     page_soup = BeautifulSoup(page_response.content, "html.parser")
     results = page_soup.find_all("div", {"class": "col-xs-12 setlistPreview"})
-    print(f"    Found {len(results)} setlists")
     songs = {}
     for result in results:
         anchor = result.find("a")
@@ -37,23 +35,23 @@ def get_artist_songs(artist: str):
         setlist_response = requests.get(setlist_url)
         setlist_soup = BeautifulSoup(setlist_response.content, "html.parser")
         song_list = setlist_soup.find_all("li", {"class": "setlistParts song"})
-        for song in song_list:
+        for position, song in enumerate(song_list, start=1):
             song_name = song.find("a", {"class": "songLabel"})
             if song_name:
                 song_name_string = song_name.get_text()
                 if song_name_string in songs:
-                    songs[song_name_string] += 1
+                    songs[song_name_string].append(position)
                 else:
-                    songs[song_name_string] = 1
-    return songs
-
-
-def get_sorted_song_list(songs: dict):
-    sorted_songs = dict(sorted(songs.items(), key=lambda x: x[1], reverse=True))
+                    songs[song_name_string] = [position]
+    for song in songs:
+        all_positions = songs[song]
+        average_postion = sum(all_positions) / len(all_positions)
+        songs[song] = average_postion
+    sorted_songs = dict(sorted(songs.items(), key=lambda x: x[1]))
     return list(sorted_songs.keys())
 
 
-def create_playlist(artist: str, songs: list, playlist_name: str):
+def create_playlist(artist: str, songs: list, playlist_name: str, market: str) -> int:
     user_id = environ["SPOTIFY_USER_ID"]
     spotify = init_spotify()
     song_uris = []
@@ -62,7 +60,7 @@ def create_playlist(artist: str, songs: list, playlist_name: str):
         search_result = spotify.search(
             q=search_query,
             type="track",
-            market="CZ",
+            market=market,
         )
         search_result_items = search_result["tracks"]["items"]
         song_uri = search_result_items[0]["uri"]
@@ -79,12 +77,15 @@ def create_playlist(artist: str, songs: list, playlist_name: str):
     spotify.user_playlist_add_tracks(
         user=user_id, playlist_id=playlist["uri"], tracks=song_uris
     )
-    print(f"Created playlist {playlist_name} with {len(song_uris)} songs")
+    return len(song_uris)
 
 
 @click.command()
 @click.option(
-    "-n", "--name", prompt="Artist Name", help="Name of the artist", multiple=True
+    "-n",
+    "--name",
+    help="Name of the artist (can be provided multiple times)",
+    multiple=True,
 )
 @click.option(
     "-pf",
@@ -92,22 +93,29 @@ def create_playlist(artist: str, songs: list, playlist_name: str):
     default="{{name}} - Most Played Live",
     help="Playlist name format. Use {{name}} to use the entered artist name. Default is: {{name}} - Most Played Live",
 )
-def main(name, playlist_format):
+@click.option(
+    "-m", "--market", default="CZ", help="Market for Spotify search. Default is CZ"
+)
+def main(name, playlist_format, market):
+    if not name:
+        print("No artist name provided, please use -n to provide one or more names")
+        print("You can also use --help to see the options")
     environment = jinja2.Environment()
     template = environment.from_string(playlist_format)
     for one_name in name:
         playlist_name = template.render(name=one_name)
+        print(f"Searching for songs by {one_name}...")
         songs = get_artist_songs(artist=one_name)
         if len(songs) > 0:
-            print(f"    Found songs played by {one_name}")
-            songs_sorted = get_sorted_song_list(songs=songs)
-            create_playlist(
-                artist=one_name, songs=songs_sorted, playlist_name=playlist_name
+            print(f"Found {len(songs)} songs played at live shows of {one_name}")
+            print("Creating playlist...")
+            final_song_count = create_playlist(
+                artist=one_name, songs=songs, playlist_name=playlist_name, market=market
             )
+            print(f"Created playlist '{playlist_name}' with {final_song_count} songs")
         else:
-            print(f"No songs found for {one_name}")
             print(
-                "    There are either no published setlists or a typo in the artist name."
+                f"No songs found for {one_name}There are either no published setlists or a typo in the artist name."
             )
 
 
